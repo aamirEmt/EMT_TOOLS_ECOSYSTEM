e746a1016ce9f6abcba3ad6e6f3343bda14ca508
abhishek-emt <abhishek.emt@easemytrip.com>
2026-02-11 12:38:16 +0530

    removed google font

--- tools_factory/hotel_cancellation/hotel_cancellation_renderer.py
+++ tools_factory/hotel_cancellation/hotel_cancellation_renderer.py
@@ -25,7 +25,6 @@ _jinja_env.filters['truncate_text'] = _truncate_text
 # =====================================================================
 INTERACTIVE_BOOKING_TEMPLATE = """
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
 .booking-details-carousel {
   font-family: poppins, sans-serif;
@@ -1134,7 +1133,6 @@ def _format_date(date_str: str) -> str:
 # =====================================================================
 CANCELLATION_SUCCESS_TEMPLATE = """
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
 .cancellation-success {
   font-family: poppins, sans-serif;
