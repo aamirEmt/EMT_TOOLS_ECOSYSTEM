c365abe78254cb88fbc8868090342b3372a3a4ec
Mayank Wadhwa <mayank.wadhwa@easemytrip.com>
2026-02-15 04:12:21 +0530

    whatsapp changes

--- tools_factory/trains/train_search_service.py
+++ tools_factory/trains/train_search_service.py
@@ -777,8 +777,7 @@ def _build_whatsapp_response_without_class(
 
         for cls in train.get("classes", []):
             class_code = cls.get("class_code", "")
-            quota_name = cls.get("quota_name", "")
-            class_label = f"{class_code}({quota_name})" if quota_name else class_code
+            class_label = f"{class_code}(Tatkal)" if cls.get("quota") == "TQ" else class_code
             classes_info.append({
                 "class": class_label,
                 "fare": cls.get("fare"),
