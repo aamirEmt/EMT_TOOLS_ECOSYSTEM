6c4102b40103228c64b92a451c4b4e21d0c298a1
Shivanya Singh <shivanya.singh@easemytrip.com>
2026-02-25 11:39:16 +0530

    old url

--- emt_client/config.py
+++ emt_client/config.py
@@ -440,7 +440,7 @@ HOTEL_SEARCH_URL = getenv("HOTEL_SEARCH_URL", HOTEL_SEARCH_URL)
 BUS_SEARCH_URL = _get_config_value(
     'BUS_SEARCH_URL',
     'BUS_SEARCH_URL',
-    default='https://busservice-p.easemytrip.com/v1/services/search/search-bus/'
+    default='https://busservice.easemytrip.com/v1/api/Home/GetSearchResult/'
 )
 
 # Bus SeatBind API endpoint
