83fd44b4ce65c9293217adf6a2ffa592f95dbdcd
aamirEmt <aamir.enam@easemytrip.com>
2026-03-06 21:33:15 +0530

    adding changes

--- emt_client/config.py
+++ emt_client/config.py
@@ -113,8 +113,7 @@ FLIGHT_ATK_TOKEN = _get_config_value(
 CHATBOT_API_BASE_URL = _get_config_value(
     'CHATBOT_API_BASE_URL',
     'CHATBOT_API_BASE_URL',
-    default='https://staging-aiml-chatbotapi.easemytrip.com'
-)
+    default='https://aiml-chatbotapi.easemytrip.com')
 
 MYBOOKINGS_BASE_URL = _get_config_value(
     'MYBOOKINGS_BASE_URL',
